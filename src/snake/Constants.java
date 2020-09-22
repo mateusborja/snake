76e1e0b72abe0c00b3d617625417e45a17d014d0
Mateus Borja <mateus.borja@gmail.com>
2020-09-21 23:01:16 -0400

    update constants class

--- src/snake/Constants.java
+++ src/snake/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
 	public static final int WINDOW_WIDTH = 1024;
 	public static final int WINDOW_HEIGHT = 768;
 	public static final String WINDOW_TITLE = "Snake Game!";
-	
+
 }
